11362e3b5028d78c2261620b879bdb57c93858da
joseph <giuseppe.falco000@gmail.com>
2020-08-26 17:39:23 +0200

    end

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 for (var i=1; i<=100; i++) {
-    if (i % 3 == 0) {
-        console.log("Fizz")
+    if ((i % 3 == 0) & (i % 5 == 0)) {
+        console.log("FizzBUzz");
     } else if (i % 5 == 0) {
         console.log("Buzz")
+    } else if (i % 3 == 0) {
+        console.log("Fizz")
     } else {
         console.log(i);
     }
